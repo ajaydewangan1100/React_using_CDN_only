6f40fbab388f4686d7ee796c9fd5cd32a06712df
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2024-08-16 15:27:10 +0530

    Created nested element using react

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-// 
-const heading = React.createElement("h1", {id:"heading", xxx:"aaa"}, "MSG From React!");
-
-console.log(heading);
-
+// Getting root
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+// creating element
+// const heading = React.createElement("h1", {id:"heading", xxx:"aaa"}, "MSG From React!");
+
+// console.log(heading);
+// root.render(heading);
+
+// Nested element structure
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "parent -> child > h1 -> I am h1")
+  )
+);
+
+// rendering element
+root.render(parent);
